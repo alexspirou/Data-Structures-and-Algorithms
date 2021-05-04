3fc3610fb72c51b61db0a0a8a65e3e60f3d3fe9a
Alex <alexspirou93@gmail.com>
2021-05-04 22:26:40 +0300

    linked list sum, min , max , display

--- 4_linked_list/src/Node.h
+++ 4_linked_list/src/Node.h
@@ -1,6 +1,7 @@
 #ifndef  LINKED_LIST_H
 #define LINKED_LIST_H
 #include <iostream>
+#include <cstring>
 template <typename T>
 class Node{
 
@@ -9,7 +10,7 @@ public:
 
 	Node(){}
 	~Node(){}
-	Node* header = NULL;
+	Node* header;
 
 	void create(T* array, int n){
 
@@ -18,7 +19,7 @@ public:
 		new_node->link = NULL;
 		Node* previous = new_node;
 		header = new_node;
-
+		count ++;
 		for(int i{1}; i < n; i++){
 			
 			new_node = new Node();
@@ -26,14 +27,76 @@ public:
 			previous = new_node;
 			new_node->data = array[i];
 			new_node->link = NULL;
+			count ++;
 		}
 
 	}
 
+	void display_node(Node* f_node){
+		std::cout << "{ ";
+		while(f_node!=NULL){
+			std::cout << f_node->data << " ";
+			f_node = f_node->link;
+		}
+		std::cout <<"}"<< std::endl;
+
+	}
+
+	size_t get_size(){
+
+		return count;
+	}
+	
+	T min(Node* f_node){
+		T min_ = f_node->data;
+
+		while (f_node!= NULL){
+			if(f_node->data < min_){
+				min_ = f_node->data;
+			}
+			f_node = f_node->link;
+		}
+		return min_;
+
+	}
+	T max(Node* f_node){
+
+		T max_ = f_node->data;
+		while(f_node!=NULL){
+			if(f_node->data > max_){
+				max_ = f_node->data;
+			}
+			f_node = f_node->link;
+		}
+		return max_;
+	}
+
+	T sum(Node* f_node){
+
+		T sum_ = f_node->data - f_node->data;
+
+		// if constexpr (std::is_same_v<T, char>){
+			
+		// 	while (f_node!=NULL){
+		// 		std::cout << "char " << std::endl;
+		// 		f_node = f_node->link;
+		// 	}	
+		// }
+		// else{	
+			while (f_node!=NULL){
+
+				sum_ = sum_ + f_node->data;
+				f_node = f_node->link;	
+			}
+
+		// }
+		return sum_;
+	}
 
 private:
 	T data;
 	Node* link;
+	size_t count {0};
 
 };
 #endif
--- 4_linked_list/src/main.cpp
+++ 4_linked_list/src/main.cpp
@@ -6,13 +6,16 @@
 
 int main(){
 
-	Node<double>* node;
+	Node<char>* node = new Node<char>();
 	int n = 5;
-	double arr [] = {1.0, 2.3, 1.6, 1.8, 2.0};
-	
+	char arr [] = {'e','l', 'a', 'a', 'a'};
+	char a = 'a';
+	char b = 'b';
 	node->create(arr, n);
 
-
+	node->display_node(node->header);
+	
+	std::cout << node->sum(node->header)<< std::endl;
 
 	return 0;
 }
